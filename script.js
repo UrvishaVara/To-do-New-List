a0f44cab0e194211d1b3a13d2a58558a5be401c7
Urvisha Vara <varaurvi@gmail.com>
2025-01-21 16:48:55 +0530

    design update

---
+++ script.js
@@ -0,0 +1,6 @@
+const todoInput = document.getElementById('todoInput');
+    const addTodoButton = document.getElementById('addTodoButton');
+    const todoList = document.getElementById('todoList');
+    const pendingCount = document.getElementById('pendingCount');
+    const completedCount = document.getElementById('completedCount');
+    const clearAllButton = document.getElementById('clearAllButton');
